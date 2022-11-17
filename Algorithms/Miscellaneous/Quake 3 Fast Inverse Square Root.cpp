4e4c52a52ca3e3e91396f223207dba2e3be4e6e4
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2022-11-17 17:22:22 +0200

    Update Quake 3 Fast Inverse Square Root.cpp

--- Algorithms/Miscellaneous/Quake 3 Fast Inverse Square Root.cpp
+++ Algorithms/Miscellaneous/Quake 3 Fast Inverse Square Root.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 float Quake3_IFSQRT(float x) {
     long i; float x2, y;
-    float threehalfs = 1.5F;
     
     x2 = x * 0.5F;
     y = x;
     i = * (long *) &y;
-    i = 0x5f3759df - (i >> 1);
+    i = 0x5f3759df - (i>>1);
     y = * (float *) &i;
-    y = y * (threehalfs - (x2*y*y));
+    y = y * (1.5F - (x2*y*y));
     return y;
 }
 
