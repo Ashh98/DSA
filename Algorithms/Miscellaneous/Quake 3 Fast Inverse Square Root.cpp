c07c493441b655c5ab4a1dfba680c86cf4d75c18
Ahmed Ashraf <36311835+Ashh98@users.noreply.github.com>
2022-11-17 17:24:53 +0200

    Add files via upload

---
+++ Algorithms/Miscellaneous/Quake 3 Fast Inverse Square Root.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+float Quake3_IFSQRT(float x) {
+    long i; float x2, y;
+    
+    x2 = x * 0.5F;
+    y = x;
+    i = * (long *) &y;
+    i = 0x5f3759df - (i>>1);
+    y = * (float *) &i;
+    y = y * (1.5F - (x2*y*y));
+    return y;
+}
+
+int main() {
+    double x = 3;
+    
+    cout << Quake3_IFSQRT(x) << endl;
+
+}
